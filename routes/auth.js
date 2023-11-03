de15aecd01ae9baf362f94d4002cd30ea2858a19
ahmedElsehrawy <aelsehrawy1999@gmail.com>
2023-11-03 13:58:08 +0200

    new commit

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,6 @@ const authController = require("../controllers/auth");
 const router = express.Router();
 
 // auth route
-
 router.get("/login", authController.getLogin);
 
 module.exports = router;
